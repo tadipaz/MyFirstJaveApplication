8ca620f30944e3e802bface9aa64ba8c01044a26
tadipaz <94741529+tadipaz@users.noreply.github.com>
2021-11-20 13:40:45 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("This is my first Java program-change");
+	System.out.println("This is my first Java program-change Changed Again!");
     }
 }
